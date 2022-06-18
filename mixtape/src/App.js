59fc4cc5047f55b0cd27456c14716146c3980356
Stephen Reish <spreish@ncsu.edu>
2022-06-18 15:58:03 -0400

    Made change to file so netlify deploys properly

--- mixtape/src/App.js
+++ mixtape/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello, world!</div>;
+  return <div className="App">Hello, world! Here is some more text so netlify stops complaining.</div>;
 }
 
 export default App;
