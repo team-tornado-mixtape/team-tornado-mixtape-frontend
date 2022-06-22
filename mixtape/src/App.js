0b94fc1ec2bae6fcb599af9ab085a91d6e404183
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-06-22 13:16:29 -0700

    added sidebar and installed mui icons

--- mixtape/src/App.js
+++ mixtape/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Landing from "./Components/Landing";
-
+import PermanentDrawerLeft from "./Components/Sidebar.jsx";
 
 function App() {
-  return <div>
-    <Landing />
-  </div>;
+  return (
+    <div>
+      {/* <Landing /> */}
+      <PermanentDrawerLeft />
+    </div>
+  );
 }
 
 export default App;
