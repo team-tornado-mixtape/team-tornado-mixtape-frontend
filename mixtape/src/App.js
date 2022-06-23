9ed2dd953dce37a047b9b8d67f38de6b057880f5
Stephen Reish <spreish@ncsu.edu>
2022-06-23 15:30:46 -0400

    Basic profile setup

--- mixtape/src/App.js
+++ mixtape/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import ComponentToggler from "./components/Landing"
 import Landing from "./components/Landing";
 import PermanentDrawerLeft from "./components/Sidebar.jsx";
+import Profile from "./components/Profile.jsx"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />}></Route>
         <Route path="/sidebar" element={<PermanentDrawerLeft />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
       </Routes>
     </BrowserRouter>
   );
