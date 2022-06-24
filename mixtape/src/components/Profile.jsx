6f220978d54e067832eb9c2d79cc89b5b0e7b057
Stephen Reish <spreish@ncsu.edu>
2022-06-24 18:48:48 -0400

    Profile looks not ugly now!

--- mixtape/src/components/Profile.jsx
+++ mixtape/src/components/Profile.jsx
@@ -5,8 +5,9 @@ import Avatar from '@mui/material/Avatar';
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import InputAdornment from '@mui/material/InputAdornment';
 import Button from "@mui/material/Button";
-
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -53,16 +54,18 @@ export default function Profile() {
                     <br></br>
                     <Typography variant="h3" style={NormalText}>Profile</Typography>
                     <br></br>
-                    <TableContainer component={Paper} sx={{ width: 400 }}>
-                        <Table component="form" sx={{ width: 400 }} aria-label="simple table">
+                    <TableContainer component={Paper} sx={{ width: 404, border: "2px solid #E2E2DF" }}>
+                        <Table component="form" sx={{ width: 400 }}>
                             <TableBody>
                                 {/* conditionally render TextFields to accept updated user input when they select "update profile" */}
+                                {/* add toggle for password visibility */}
+                                {/* standardize small TextFields across app */}
                                 {editingMode === 'false' ? (
                                     <>
-                                        {/* set background color of first tablerow, and text color in cells, based on avi  */}
+                                        {/* set background color of first tablerow based on avi  */}
                                         <TableRow sx={{ backgroundColor: "#8F4F6A" }}>
                                             <TableCell align="left">
-                                                <Avatar sx={{ width: 70, height: 70 }} alt="Avatar Aang" src="https://sportshub.cbsistatic.com/i/2022/03/01/fdf9fb59-929a-42c1-aa1e-4af3d16f2339/avatar-aang-cosplay.jpg" />
+                                                <Avatar sx={{ width: 70, height: 70, border: "2.5px solid #E2E2DF" }} alt="Avatar Aang" src="https://sportshub.cbsistatic.com/i/2022/03/01/fdf9fb59-929a-42c1-aa1e-4af3d16f2339/avatar-aang-cosplay.jpg" />
                                             </TableCell>
                                             <TableCell align="right">
                                                 <Stack spacing={1} direction="column">
@@ -90,28 +93,55 @@ export default function Profile() {
                                     </>
                                 ) : (
                                     <>
-                                        <TableRow>
+                                        <TableRow sx={{ backgroundColor: "#8F4F6A" }}>
                                             <TableCell align="left">
-                                                <Typography variant="p" style={InfoText}>username</Typography>
+                                                <Avatar sx={{ width: 70, height: 70 }} alt="Avatar Aang" src="https://sportshub.cbsistatic.com/i/2022/03/01/fdf9fb59-929a-42c1-aa1e-4af3d16f2339/avatar-aang-cosplay.jpg" />
                                             </TableCell>
                                             <TableCell align="right">
-                                                <TextField label="update username" defaultValue="avataraang" />
+                                                <Stack spacing={1} direction="column">
+                                                    <Typography variant="h3" style={NormalText}>Aang</Typography>
+                                                    <Typography variant="h6" style={NormalText}>@avataraang</Typography>
+                                                </Stack>
                                             </TableCell>
                                         </TableRow>
                                         <TableRow>
                                             <TableCell align="left">
-                                                <Typography variant="p" style={InfoText}>password</Typography>
+                                                <Typography variant="p" style={InfoText}>current username</Typography>
                                             </TableCell>
                                             <TableCell align="right">
-                                                <TextField type="password" label="update password" />
+                                                <Typography variant="p" style={NormalText}>avataraang</Typography>
                                             </TableCell>
                                         </TableRow>
                                         <TableRow>
                                             <TableCell align="left">
-                                                <Typography variant="p" style={InfoText}>password</Typography>
+                                                <Typography variant="p" style={InfoText}>change username</Typography>
                                             </TableCell>
                                             <TableCell align="right">
-                                                <TextField type="password" label="confirm new password" />
+                                                <TextField size="small" defaultValue="avataraang" />
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell align="left">
+                                                <Typography variant="p" style={InfoText}>current password</Typography>
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                <Typography variant="p" style={NormalText}>updated: 3 june 2022</Typography>
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell align="left">
+                                                <Typography variant="p" style={InfoText}>change password</Typography>
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                <TextField size="small" type="password" />
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell align="left">
+                                                <Typography variant="p" style={InfoText}>confirm new password</Typography>
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                <TextField size="small" type="password" />
                                             </TableCell>
                                         </TableRow>
                                     </>
@@ -122,12 +152,12 @@ export default function Profile() {
                     <br></br>
                     {editingMode === 'false' ? (
                         <Stack spacing={1} direction="row">
-                            <Button variant="outlined" onClick={() => enterEditingMode()}> Update Info</Button>
+                            <Button variant="contained" onClick={() => enterEditingMode()}> Edit Profile</Button>
                         </Stack>
                     ) : (
                         <Stack spacing={1} direction="row">
-                            <Button variant="outlined" onClick={() => cancelEditingMode()}>Cancel</Button>
-                            <Button variant="contained" onClick={() => saveEdit()}>Update</Button>
+                            <Button variant="contained" color="secondary" onClick={() => cancelEditingMode()}>Cancel</Button>
+                            <Button variant="contained" onClick={() => saveEdit()}>Save</Button>
                         </Stack>
                     )}
                 </Box>
