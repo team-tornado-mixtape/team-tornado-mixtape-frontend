7ed6228f26c37eccbc6e4edfa762dda0f0277fb1
Stephen Reish <spreish@ncsu.edu>
2022-06-27 19:21:48 -0400

    got search working and preview (partially) working

--- mixtape/src/components/MixCreate.jsx
+++ mixtape/src/components/MixCreate.jsx
@@ -11,6 +11,20 @@ import Stack from "@mui/material/Stack";
 
 import { Link } from "react-router-dom";
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+
+
 export default function MixCreate({ setAuth, isLoggedIn, token, username }) {
   const [mixtapeTitle, setMixtapeTitle] = useState('')
   const [mixtapeDescription, setMixtapeDescription] = useState('')
@@ -18,6 +32,10 @@ export default function MixCreate({ setAuth, isLoggedIn, token, username }) {
   // const [trackList, setTrackList] = useState([])
   const [error, setError] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
+
+  const NormalText = {
+    userSelect: "none",
+  }
 
   if (!isLoggedIn) {
     return <Navigate to="/" replace={true} />
@@ -48,7 +66,6 @@ export default function MixCreate({ setAuth, isLoggedIn, token, username }) {
 
   return (
     <>
-      <Typography variant="p" color="secondary">By the time this page has been rendered, the POST request for creating a mixtape (POST /mixtapes/) has been called and we have recieved a response that the mixtape was created. We could construct a new GET request (GET /mixtapes/id/) to display the newly created mixtape information on this page. (How will we know the mixtape ID after the initial POST? Can this be given to us in the response from the initial POST?)</Typography>
       <Box sx={{ textAlign: "center", justifyContent: "center", border: "1px solid white" }}>
         <Stack spacing={2} direction="column">
           <TextField
@@ -67,7 +84,6 @@ export default function MixCreate({ setAuth, isLoggedIn, token, username }) {
           />
         </Stack>
       </Box>
-      <Typography variant="p" color="secondary">The fields above are populated with default values the user may now change. Any changes they make would be kept in state until the user presses 'save mixtape.'</Typography>
       <Box sx={{ textAlign: "left", justifyContent: "center", border: "1px solid white" }}>
         <Stack spacing={10} direction="row">
           <TextField
@@ -81,31 +97,66 @@ export default function MixCreate({ setAuth, isLoggedIn, token, username }) {
             Search
           </Button>
         </Stack>
-        <Typography variant="p">Song search results</Typography>
-        <Box
-          sx={{
-            width: "90%",
-            height: 350,
-            border: "1px solid white"
-          }}
-        ><br></br>
-          <Typography variant="p">search results will be in a table here. next to each result will be an add button, which, if clicked, will add the song to the mixtape's tracklist.</Typography></Box>
-      </Box>
+        <Stack spacing={10} direction="row">
+          <Stack spacing={2} direction="column">
+            <Typography variant="p">Song search results</Typography>
+            <TableContainer component={Paper} sx={{ width: "45vw", border: "2px solid #E2E2DF" }}>
+              <Table component="form">
+                <TableBody>
+                  {allResults.map((eachResult, index) => {
+                    return (
+                      <TableRow key={index}>
+                        <TableCell align="left">
+                          <IconButton href={eachResult.preview_url} sx={{ color: "#FFFFFF" }}>
+                            <PlayCircleOutlineIcon />
+                          </IconButton>
+                        </TableCell>
+                        <TableCell align="left">
+                          <Typography variant="h5">{eachResult.title}</Typography>
+                        </TableCell>
+                        <TableCell align="left">
+                          <Typography variant="h5">{eachResult.artist}</Typography>
+                        </TableCell>
+                        <TableCell align="right">
+                          <IconButton sx={{ color: "#FFFFFF" }}>
+                            <AddCircleOutlineIcon />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    )
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Stack>
+          <Stack spacing={2} direction="column">
+            <Typography variant="p">Mixtape tracklist</Typography>
+            <TableContainer component={Paper} sx={{ width: 404, border: "2px solid #E2E2DF" }}>
+              <Table component="form" sx={{ width: 400 }}>
+                <TableBody>
+                  <TableRow>
+                    <TableCell align="left">
+                      <IconButton sx={{ color: "#FFFFFF" }}>
+                        <PlayCircleOutlineIcon />
+                      </IconButton>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Typography variant="h5">Song title</Typography>
+                    </TableCell>
+                    <TableCell align="right">
+                      <IconButton sx={{ color: "#FFFFFF" }}>
+                        <AddCircleOutlineIcon />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Stack>
+        </Stack>
+      </Box >
       <br></br>
       <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <Stack direction="column">
-          <Typography variant="p">Mixtape Tracklist</Typography>
-          <Box
-            sx={{
-              width: "90%",
-              height: 350,
-              border: "1px solid white"
-            }}>
-            <br></br>
-            <Typography variant="p">results the user has added to their tracklist will be in a table here. next to each track will be a remove button, which, if clicked, will remove the song from the mixtape's tracklist, and out of this table.</Typography>
-          </Box>
-          <Typography variant="p" color="secondary">The 'song search results' view is populated with the results from the search (GET /search?track=track+title). When the user clicks the add buttom from within the 'song search results' view, the selected track is immediately PATCHed (PATCH /mixtapes/id/) with that song added in the request body. The same logic would be used when a user decides to remove a track from their tracklist.</Typography>
-        </Stack>
         <Box>
           <Typography variant="p">Customization</Typography>
           <br></br>
@@ -140,7 +191,6 @@ export default function MixCreate({ setAuth, isLoggedIn, token, username }) {
               }}
             ></Box>
             <br></br>
-            <Typography variant="p" color="secondary">Like the track name and description, changes to the theme and the uploading of a photo would be kept in state, until the user presses 'save mixtape'. Then, a PATCH would take place (PATCH /mixtapes/id/) which updates the mixtape's non-song metadata.</Typography>
           </Stack>
         </Box>
       </Box>
