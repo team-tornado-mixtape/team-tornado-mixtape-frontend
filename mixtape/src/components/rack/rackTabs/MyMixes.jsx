1f11b6f398753948657c89c151d75461eee77b73
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-06-30 14:55:22 -0700

    rack my mixes works

--- mixtape/src/components/rack/rackTabs/MyMixes.jsx
+++ mixtape/src/components/rack/rackTabs/MyMixes.jsx
@@ -9,49 +9,41 @@ import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { Link } from "react-router-dom";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 
-export default function MyMixes({ setAuth, isLoggedIn, token, username, }){
-
-  const [ myMixes, setMyMixes ] = useState([])
-  const [error, setError] = useState('')
+export default function MyMixes({ setAuth, isLoggedIn, token, username }) {
+  const [myMixes, setMyMixes] = useState([]);
+  const [error, setError] = useState("");
 
   const NormalText = {
-  userSelect: "none",
-  }
+    userSelect: "none",
+  };
+  useEffect(() => {
+    axios
 
-  // const rackList = {
-axios
-  
-    .get(
-      `https://team-tornado-mixtape.herokuapp.com/api/my/mixtapes/`,
-      {
+      .get(`https://team-tornado-mixtape.herokuapp.com/api/my/mixtapes/`, {
         headers: { Authorization: `Token ${token}` },
-      }
-    )
-    .then((res) => {
-      console.log(res.status)
-      console.log(res.data)
-      setMyMixes(res.data)
-      // setIsLoading(false)
-    })
-    .catch((e) => {
-      setError(e.message)
-      // setIsLoading(false)
-    })
-  console.log(error)
-  // }
+      })
+      .then((res) => {
+        console.log(res.status);
+        console.log(res.data);
+        setMyMixes(res.data);
+        // setIsLoading(false)
+      })
+      .catch((e) => {
+        setError(e.message);
+        // setIsLoading(false)
+      });
+    console.log(error);
+    // }
+  }, [token, error]);
 
-    return (
-      <>
-      <Box>Fuckery</Box>
-        {myMixes.map((eachMix, index) => {
-          return (
-            <Box key={index}>
-              
-            </Box>
-          )
-        })}
-      </>
-    )
-  }
+  return (
+    <>
+      <Box></Box>
+      {myMixes.map((eachMix, index) => {
+        return <Box key={index}></Box>;
+      })}
+    </>
+  );
+}
